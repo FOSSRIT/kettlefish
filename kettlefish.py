c4b80d23f7694b58a121c286e18cabdd7a5e61d2
Nathaniel Case <Qalthos@gmail.com>
2013-07-16 17:47:55 -0400

    make sure remyspoken words are on word boundaries.

--- kettlefish.py
+++ kettlefish.py
@@ -3,6 +3,7 @@
 
 import argparse
 import os
+import re
 import string
 
 
@@ -70,7 +71,7 @@ def translate_remyspeak(text):
 
     text = text.lower()
     for item in REMYSPEAK:
-        text = text.replace(item, REMYSPEAK[item])
+        text = re.sub(r'\b{}\b'.format(item), REMYSPEAK[item], text)
 
     return cap_map[case](text)
 
