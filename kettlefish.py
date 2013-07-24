bb9a9701b60487c8f9d755d1ef28962c24ace466
Nathaniel Case <Qalthos@gmail.com>
2013-07-24 19:14:59 -0400

    Don't fail if there is no text.

--- kettlefish.py
+++ kettlefish.py
@@ -72,6 +72,8 @@ REMYSPEAK.update({
 def translate_remyspeak(text):
     cap_map = [str.lower, str.capitalize, str.upper]
     # figure out casing of input
+    if not text:
+        return text
     if text[0] in string.lowercase:
         case = 0
     elif text[1] in string.lowercase:
