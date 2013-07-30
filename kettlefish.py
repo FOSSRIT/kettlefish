9b009082e4a40a0cc5c7f604483711314bf9f5a6
David Gay <oddshocks@riseup.net>
2013-07-30 11:46:48 -0400

    Fix #14 ("minute" --> "a minute" for "long time" --> "a long time")

--- kettlefish.py
+++ kettlefish.py
@@ -56,7 +56,7 @@ REMYSPEAK.update({
     "lolz": "*giggle*",
     "lolol": "haha",
     "lollerskates": "hilarious",
-    "minute": "long time",
+    "a minute": "a long time",
     "lo(ad|de)stone": "bad luck",
     "beverage(s)?": "alcohol",
     "may or may not be": "is",
